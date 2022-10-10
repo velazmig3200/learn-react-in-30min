8ed39acf3912664aece507bd14f36859e8c4a24b
velazmig3200 <82695995+velazmig3200@users.noreply.github.com>
2022-10-10 15:38:26 -0500

    initial commit
    "Learn React in 30 Minutes" project by Web Dev Simplified, on YouTube.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+
+let i = 0;
+const LSKey = 'todoApp';//local storage key
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LSKey));
+    if (storedTodos == null) {return;}
+    if (storedTodos.length > 0) {setTodos(storedTodos);
+    console.log('useEffect, get');}
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LSKey, JSON.stringify(todos));
+    console.log('useEffect, set');
+  }, [todos])
+
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === ' ') return; 
+    setTodos(prevTodos => {
+      ++i;
+      return [...prevTodos, {id: Math.random(), name: name, complete: false}];
+    });
+    todoNameRef.current.value = null;
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete);
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" /> 
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Completed</button>
+      <div>{todos.filter(todo => !todo.complete).length} left Todos</div>
+    </>
+  )
 }
 
 export default App;
